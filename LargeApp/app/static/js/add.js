ea69dfb94030b5f73fda0d8933291d72ffab0acb
t-soumbou <terrencesoumbou@gmail.com>
2018-02-08 22:09:33 +0100

    first update

---
+++ LargeApp/app/static/js/add.js
@@ -0,0 +1,16 @@
+function toggle(anId)
+{
+	node = document.getElementById(anId);
+	if (node.style.visibility=="hidden")
+	{
+		// Contenu caché, le montrer
+		node.style.visibility = "visible";
+		node.style.height = "auto";			// Optionnel rétablir la hauteur
+	}
+	else
+	{
+		// Contenu visible, le cacher
+		node.style.visibility = "hidden";
+		node.style.height = "0";			// Optionnel libérer l'espace
+	}
+}
